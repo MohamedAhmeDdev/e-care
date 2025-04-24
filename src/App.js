370a3ad1d2627596745a8546e7078c6b90a2bbec
mohamed ahmed <ma07041705@gmail.com>
2025-04-24 19:48:27 +0300

    Refactor App component to implement routing with BrowserRouter and organize routes for authentication and dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter , Route, Routes } from "react-router-dom";
+import "./App.css";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/Dashboard";
+import Clients from "./pages/Clients/Clients";
+import RegisterClient from "./pages/Clients/RegisterClient";
+import EnrollClient from "./pages/Clients/EnrollClient";
+import ClientProfile from "./pages/Clients/ClientProfile";
+import Programs from "./pages/Program/Programs";
+import CreateProgram from "./pages/Program/CreateProgram";
+import ClientsPrograms from "./pages/Clients/ClientsPrograms";
+import UpdateProgram from "./pages/Program/UpdateProgram";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+    <BrowserRouter>
+      <Routes>
+        {/* public auth pages */}
+        <Route path="register" element={<Register />} />
+        <Route path="login"    element={<Login />} />
+
+        {/* protected dashboard layout */}
+        <Route path="/" element={<DashboardLayout />}>
+          {/* Dashboard “index” view */}
+          <Route index element={<Dashboard />} />
+
+          {/* client routes */}
+          <Route path="clients" element={<Clients />} />
+          <Route path="clients/new" element={<RegisterClient />} />
+          <Route path="clients/:id/enroll" element={<EnrollClient />} />
+          <Route path="clients/:id/programs" element={<ClientsPrograms />} />
+          <Route path="clients/:id/profile" element={<ClientProfile />} />
+
+          {/* program routes */}
+          <Route path="programs" element={<Programs />} />
+          <Route path="programs/new" element={<CreateProgram />} />
+          <Route path="programs/:id/edit" element={<UpdateProgram />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+	);
 }
 
 export default App;
